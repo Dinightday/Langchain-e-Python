e8f2603dae0e2bbdb369d0cc6b057001b604bc54
dinightday <darkrip1000@gmail.com>
2026-02-07 00:29:32 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+from pydantic import Field, BaseModel
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 import os
 
 load_dotenv()
 
 api_key = os.environ["OPENAI_API_KEY"]
+
+class Destino(BaseModel):
+    cidade: str = Field("A cidade a qual foi indicada.")
+    motivo: str = Field("O motivo para essa cidade ser escolhida")
+
+paseador = JsonOutputParser(pydantic_object=Destino)
 
 llm = ChatOpenAI(api_key=api_key, model="gpt-3.5-turbo", temperature=0.8)
 
@@ -12,8 +21,19 @@ numero_dia = 7
 numero_crianca = 2
 atividade = "musica"
 
-prompt = f"Crie um roteiro de viagem de {numero_dia} dias, para uma familia com {numero_crianca} crianças e no carro indique uma {atividade}."
+prompt = PromptTemplate.from_template(
+"""Crie um roteiro de viagem de {numero_di} dias, 
+para uma familia com {numero_crianc} crianças e 
+indique uma atividade: {atividad}.
 
-response = llm.invoke(prompt)
+{formato_saida}
+""")
 
-print(response.content)
+chain = prompt | llm | StrOutputParser()
+
+response = chain.invoke({"numero_di": numero_dia, 
+                         "numero_crianc": numero_crianca, 
+                         "atividad": atividade,
+                         "formato_saida": paseador.get_format_instructions()})
+
+print(response)
