15d2cc35a0f1b987e97bfc7827d2c5eb4c0450fd
dinightday <darkrip1000@gmail.com>
2026-02-06 23:35:13 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
@@ -6,7 +6,7 @@ load_dotenv()
 
 api_key = os.environ["OPENAI_API_KEY"]
 
-llm = OpenAI(api_key=api_key)
+llm = ChatOpenAI(api_key=api_key, model="gpt-3.5-turbo", temperature=0.8)
 
 numero_dia = 7
 numero_crianca = 2
@@ -14,17 +14,6 @@ atividade = "musica"
 
 prompt = f"Crie um roteiro de viagem de {numero_dia} dias, para uma familia com {numero_crianca} crianças e no carro indique uma {atividade}."
 
-response = llm.chat.completions.create(
-    model='gpt-3.5-turbo',
-    messages=[
-        {
-            "role": "system",
-            "content": "Você é um assistente de viagem e quero que você crie itinerarios. Sem nenhuma outra complementação. Com base no prompt"
-        },
-        {
-            "role": "user",
-            "content": prompt
-        }
-    ]
-)
-print(response.choices[0].message.content)
+response = llm.invoke(prompt)
+
+print(response.content)
